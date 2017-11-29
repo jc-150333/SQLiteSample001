b4360957d94294347d681d3a897e3d86e8854a3c
150333 <150333@jc-21.jp>
2017-11-29 16:28:56 +0900

    ボタン

--- LinqSample001/LinqSample001/LinqSamplePage008.xaml.cs
+++ LinqSample001/LinqSample001/LinqSamplePage008.xaml.cs
@@ -33,7 +33,34 @@ namespace LinqSample001
             UserModel008.insertUser("田中");
             UserModel008.insertUser("斎藤");
 
-            var buttonAdd = new Button
+            var button = new Button { Text = "Load" };
+
+            var content = new ContentPage
+            {
+                Title = "SQLite",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        button
+                    }
+                }
+            };
+
+            button.Clicked += async (sender, args) =>
+            {
+                var query = UserModel008.selectUser();
+
+                foreach (var user in query)
+                {
+
+                    //Userテーブルの名前列をLabelに書き出す
+                    layout.Children.Add(new Label { Text = user.Name });
+                }
+            };
+
+            /*var buttonAdd = new Button
             {
                 WidthRequest = 60,
                 TextColor = Color.White,
@@ -41,6 +68,7 @@ namespace LinqSample001
             };
 
             layout.Children.Add(buttonAdd);
+            
 
             //Userテーブルの行データを取得
             var query = UserModel008.selectUser();
@@ -51,6 +79,7 @@ namespace LinqSample001
                 //Userテーブルの名前列をLabelに書き出す
                 layout.Children.Add(new Label { Text = user.Name });
             }
+            */
 
             Content = layout;
         }
